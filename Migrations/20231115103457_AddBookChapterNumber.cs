6d5af70b1f7edc683e546bca7cd818b81f2e1018
Yura <you@example.com>
2023-11-15 13:38:15 +0300

    add bookChapter

---
+++ Migrations/20231115103457_AddBookChapterNumber.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthorVerseServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookChapterNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_AspNetUsers_UserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_AspNetUsers_User2Id",
+                table: "Friendships");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookChapterNumber",
+                table: "BookChapters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships",
+                columns: new[] { "User1Id", "User2Id", "Status" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_AspNetUsers_User1Id",
+                table: "Friendships",
+                column: "User1Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_AspNetUsers_User2Id",
+                table: "Friendships",
+                column: "User2Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_AspNetUsers_User1Id",
+                table: "Friendships");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_AspNetUsers_User2Id",
+                table: "Friendships");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Friendships",
+                table: "Friendships");
+
+            migrationBuilder.DropColumn(
+                name: "BookChapterNumber",
+                table: "BookChapters");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "AspNetUsers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserId",
+                table: "AspNetUsers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_AspNetUsers_UserId",
+                table: "AspNetUsers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_AspNetUsers_User2Id",
+                table: "Friendships",
+                column: "User2Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
